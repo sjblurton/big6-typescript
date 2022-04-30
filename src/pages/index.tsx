39f740c5914c930357059989c604c37a3857d534
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-30 08:05:45 +0100

    refactor: auth login cleanned up.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,19 @@
 import type { NextPage } from "next";
 import { useContext } from "react";
 import Seo from "../components/layout/seo/seo";
-import { auth, googleLogin, signOut } from "../config/firebase.config";
+import { auth, signOut } from "../config/firebase.config";
 import { useAuth } from "../hooks/use-auth";
-import { ActionType } from "../state/actions";
+import { AuthService } from "../services/auth";
 import { AuthContext } from "../state/auth-context";
 
 const Home: NextPage = () => {
   const { state, dispatch } = useContext(AuthContext);
   useAuth();
 
-  const handleClick = async () => {
-    dispatch({ type: ActionType.UpdateStatus, payload: "loading" });
-    try {
-      await googleLogin();
-      dispatch({ type: ActionType.UpdateStatus, payload: "success" });
-    } catch (error: any) {
-      dispatch({ type: ActionType.UpdateStatus, payload: "error" });
-      dispatch({
-        type: ActionType.AuthErrors,
-        payload: { message: error?.message, code: error?.code },
-      });
-    }
-  };
-
   if (state.status === "loading") {
     return <h1>Loading...</h1>;
   }
+
   return (
     <div>
       <Seo title="Home" />
@@ -41,7 +28,10 @@ const Home: NextPage = () => {
             error: {state.error?.message + " " + state.error?.code ?? "none"}
           </h2>
         }
-        <button disabled={state.user !== null} onClick={handleClick}>
+        <button
+          disabled={state.user !== null}
+          onClick={() => AuthService.handleGoogle(dispatch)}
+        >
           login with google
         </button>
         <button disabled={state.user === null} onClick={() => signOut(auth)}>
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,19 @@
+import { Dispatch } from "react";
+import { googleLogin } from "../config/firebase.config";
+import { ActionType, AuthActions } from "../state/actions";
+
 export const AuthService = {
-  handleGoogle: async () => {},
+  handleGoogle: async (dispatch: Dispatch<AuthActions>) => {
+    dispatch({ type: ActionType.UpdateStatus, payload: "loading" });
+    try {
+      await googleLogin();
+      dispatch({ type: ActionType.UpdateStatus, payload: "success" });
+    } catch (error: any) {
+      dispatch({ type: ActionType.UpdateStatus, payload: "error" });
+      dispatch({
+        type: ActionType.AuthErrors,
+        payload: { message: error?.message, code: error?.code },
+      });
+    }
+  },
 };
